9d511c9ffd02e13da16a358c01a4c97c7fd82f7d
PhD20 <dmprofessor227@gmail.com>
2023-06-27 14:59:53 -0500

    Add more allowed tags and iframe + reformat

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,13 @@
-import rss from "@astrojs/rss";
-import { blog } from "../lib/markdoc/frontmatter.schema";
-import { readAll } from "../lib/markdoc/read";
-import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from "../config";
-import Markdoc from "@markdoc/markdoc";
+import rss from '@astrojs/rss';
+import { blog } from '../lib/markdoc/frontmatter.schema';
+import { readAll } from '../lib/markdoc/read';
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from '../config';
+import Markdoc from '@markdoc/markdoc';
 import sanitizeHtml from 'sanitize-html';
 
 export const get = async () => {
   const posts = await readAll({
-    directory: "blog",
+    directory: 'blog',
     frontmatterSchema: blog,
   });
 
@@ -22,39 +22,51 @@ export const get = async () => {
   let baseUrl = SITE_URL;
   // removing trailing slash if found
   // https://example.com/ => https://example.com
-  baseUrl = baseUrl.replace(/\/+$/g, "");
+  baseUrl = baseUrl.replace(/\/+$/g, '');
 
-  const rssItems = sortedPosts.map(({ frontmatter, slug, content: postContent }) => {
-    if (frontmatter.external) {
+  const rssItems = sortedPosts.map(
+    ({ frontmatter, slug, content: postContent }) => {
+      if (frontmatter.external) {
+        const title = frontmatter.title;
+        const pubDate = frontmatter.date;
+        const link = frontmatter.url;
+
+        return {
+          title,
+          pubDate,
+          link,
+        };
+      }
+
       const title = frontmatter.title;
       const pubDate = frontmatter.date;
-      const link = frontmatter.url;
+      const description = frontmatter.description;
+      const link = `${baseUrl}/blog/${slug}`;
+      const content = sanitizeHtml(Markdoc.renderers.html(postContent), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+          'img',
+          'pre',
+          'h1',
+          'h2',
+          'h3',
+          'h4',
+          'h5',
+          'h6',
+        ]),
+        allowedAttributes: { img: ['src'] },
+        allowedSchemes: ['data', 'http', 'https'],
+        allowedIframeHostnames: ['www.youtube.com', 'www.twitter.com'],
+      });
 
       return {
         title,
         pubDate,
+        description,
         link,
+        content,
       };
     }
-
-    const title = frontmatter.title;
-    const pubDate = frontmatter.date;
-    const description = frontmatter.description;
-    const link = `${baseUrl}/blog/${slug}`;
-    const content = sanitizeHtml(Markdoc.renderers.html(postContent), {
-     allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ]),
-     allowedAttributes: {'img': ['src']},
-     allowedSchemes: [ 'data', 'http', 'https']
-}); 
-
-    return {
-      title,
-      pubDate,
-      description,
-      link,
-      content
-    };
-  });
+  );
 
   return rss({
     title: SITE_TITLE,
